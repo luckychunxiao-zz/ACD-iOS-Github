0cea543016024f7bc7b1d0463ab0fd560346b658
Kevin Li <v-lchunx@microsoft.com>
2018-05-08 14:08:56 +0800

    Init

---
+++ ACD-iOS-Github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ACD-iOS-Github
+//
+//  Created by AppCenterWX on 2018/5/8.
+//  Copyright © 2018 AppCenterWX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ACD-iOS-Github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ACD-iOS-Github
+//
+//  Created by AppCenterWX on 2018/5/8.
+//  Copyright © 2018 AppCenterWX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
